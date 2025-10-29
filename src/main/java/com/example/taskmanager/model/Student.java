9d4ff65c7ff988fdf0eeea70194687f7255d319a
Jocasta-spec <camtu7092003@gmail.com>
2025-10-29 15:04:39 +0700

    fix update qly khoa hoc - update 3 model + ham updateclass trong apiservice + showclassdialog trong coursemanagementpanel Tu 29/10/25

--- src/main/java/com/example/taskmanager/model/Student.java
+++ src/main/java/com/example/taskmanager/model/Student.java
@@ -1,15 +1,28 @@
 package com.example.taskmanager.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDateTime;
 
 public class Student {
+    @JsonProperty("Id")
     private Long id;
+
+    @JsonProperty("IdAccount")
     private Long idAccount;
+
+    @JsonProperty("Name")
     private String name;
+
+    @JsonProperty("ClassId")
     private Long classId;
+
+    @JsonProperty("CreateDate")
     private LocalDateTime createDate;
+
+    @JsonProperty("UpdateDate")
     private LocalDateTime updateDate;
 
+    // Constructors
     public Student() {}
 
     public Student(Long id, String name, Long classId) {
@@ -18,17 +31,22 @@ public class Student {
         this.classId = classId;
     }
 
+    // Getters & Setters
     public Long getId() { return id; }
-    public Long getIdAccount() { return idAccount; }
-    public String getName() { return name; }
-    public Long getClassId() { return classId; }
-    public LocalDateTime getCreateDate() { return createDate; }
-    public LocalDateTime getUpdateDate() { return updateDate; }
-
     public void setId(Long id) { this.id = id; }
+
+    public Long getIdAccount() { return idAccount; }
     public void setIdAccount(Long idAccount) { this.idAccount = idAccount; }
+
+    public String getName() { return name; }
     public void setName(String name) { this.name = name; }
+
+    public Long getClassId() { return classId; }
     public void setClassId(Long classId) { this.classId = classId; }
+
+    public LocalDateTime getCreateDate() { return createDate; }
     public void setCreateDate(LocalDateTime createDate) { this.createDate = createDate; }
+
+    public LocalDateTime getUpdateDate() { return updateDate; }
     public void setUpdateDate(LocalDateTime updateDate) { this.updateDate = updateDate; }
 }
